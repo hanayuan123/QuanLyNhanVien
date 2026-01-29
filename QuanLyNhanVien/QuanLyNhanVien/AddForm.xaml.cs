2d7536b35d2b10fb4ef0247a3a85252b95372d7b
thoconcute1307 <thoconcute1307@gmail.com>
2026-01-29 15:24:44 +0700

    them add form va edit form do Khong Minh Khoa lm

---
+++ QuanLyNhanVien/QuanLyNhanVien/AddForm.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+// Author: Khổng Minh Khoa
+// Mô tả: Lớp xử lý cho màn hình Thêm Nhân Viên
+namespace QuanLyNhanVien
+{
+    /// <summary>
+    /// Interaction logic for AddForm.xaml
+    /// </summary>
+    public partial class AddForm : Window
+    {
+        public AddForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ QuanLyNhanVien/QuanLyNhanVien/EditForm.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+// Author: Khổng Minh Khoa
+// Mô tả: Lớp xử lý cho màn hình Chỉnh Nhân Viên
+
+namespace QuanLyNhanVien
+{
+    /// <summary>
+    /// Interaction logic for EditForm.xaml
+    /// </summary>
+    public partial class EditForm : Window
+    {
+        public EditForm()
+        {
+            InitializeComponent();
+        }
+    }
+}

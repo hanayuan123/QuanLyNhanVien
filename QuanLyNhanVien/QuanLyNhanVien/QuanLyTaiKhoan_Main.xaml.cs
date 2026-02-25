9a359ad48d5fa82449f2dfc657bd0e8ea3d7a20e
Đặng Quang Trường <quangtruongdang0304@gmail.com>
2026-02-25 16:07:33 +0700

    Quan ly tai khoan

---
+++ QuanLyNhanVien/QuanLyNhanVien/QuanLyTaiKhoan_Main.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyNhanVien
+{
+    /// <summary>
+    /// Interaction logic for QuanLyTaiKhoan_Main.xaml
+    /// </summary>
+    public partial class QuanLyTaiKhoan_Main : Window
+    {
+        public QuanLyTaiKhoan_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            var addWindow = new QuanLyTaiKhoan_Them();
+            addWindow.Show();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
